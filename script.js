e07ec93a7e483548a6f0a6f077b85d41acb35175
Marc Joyce <marcjoyce@googlemail.com>
2020-03-03 22:33:38 +0000

    Update to index.html, styles.css, bg.png added logo.png removed lindenTreeLogo(1).png
    updates following colaboration with client

--- script.js
+++ script.js
@@ -1,40 +1,9 @@
-
-$(function () { });
-
-$(document).ready(function () {
-    $("#toggle").click(function () {
-        $(this).toggleClass("on");
-        $("#resize").toggleClass("active");
-    });
-    $("#resize ul li a").click(function () {
-        $(this).toggleClass("on");
-        $("#resize").toggleClass("active");
-    });
-    $(".close-btn").click(function () {
-        $(this).toggleClass("on");
-        $("#resize").toggleClass("active");
-    });
-
-    $(".animate-fadeInUp").each(function (i) {
-        var bottom_of_element =
-            $(this).position().top + $(this).outerHeight() / 3;
-        var bottom_of_window = $(window).scrollTop() + $(window).height();
-
-        if (bottom_of_window > bottom_of_element) {
-            $(this).animate({ opacity: "1" }, 100);
-        }
-    });
+var show = document.getElementById("nav-links");
 
 
-    $(window).scroll(function () {
-        $(".animate-fadeInUp").each(function (i) {
-            var bottom_of_element =
-                $(this).position().top + $(this).outerHeight() / 3;
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-
-            if (bottom_of_window > bottom_of_element) {
-                $(this).animate({ opacity: "1" }, 1000);
-            }
-        });
-    });
-});
+function showMenu() {
+    show.style.right = "0";
+} 
+function closeMenu() {
+    show.style.right = "-200px";
+}
